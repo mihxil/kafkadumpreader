63618fb01226951478ea041a52fea0406046eb3c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-02-07 12:09:08 +0100

    Testing only.

---
+++ src/test/java/org/meeuw/util/kafka/KafkaDumpReaderTest.java
@@ -0,0 +1,17 @@
+package org.meeuw.util.kafka;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+class KafkaDumpReaderTest {
+
+    @Test
+    public void test() {
+        Stream<KafkaDumpReader.Record> read = KafkaDumpReader.read(getClass().getResourceAsStream("/availability/availability-messages.table"));
+        read.forEach(r -> {
+            System.out.println(r.toString() + " " + r.key() + "=" + r.offset());
+        });
+    }
+
+}
